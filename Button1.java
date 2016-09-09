63393cde4abfba79a1fa42059c0dedf54a8ee8f0
Ly Nguyen <Ly900@aol.com>
2016-09-09 00:26:37 -0400

    Add a button to each region.

--- Button1.java
+++ Button1.java
@@ -10,10 +10,22 @@ public class Button1 {
 
   public void go() {
     JFrame frame = new JFrame();
-    JButton button = new JButton("Click me.");
+    JButton button1 = new JButton("West");
+    JButton button2 = new JButton("East");
+    JButton button3 = new JButton("North");
+    JButton button4 = new JButton("South");
+    JButton button5 = new JButton("Center");
     Font bigFont = new Font("serif", Font.BOLD, 28);
-    button.setFont(bigFont);
-    frame.getContentPane().add(BorderLayout.NORTH, button);
+    button1.setFont(bigFont);
+    button2.setFont(bigFont);
+    button3.setFont(bigFont);
+    button4.setFont(bigFont);
+    button5.setFont(bigFont);
+    frame.getContentPane().add(BorderLayout.WEST, button1);
+    frame.getContentPane().add(BorderLayout.EAST, button2);
+    frame.getContentPane().add(BorderLayout.NORTH, button3);
+    frame.getContentPane().add(BorderLayout.SOUTH, button4);
+    frame.getContentPane().add(BorderLayout.CENTER, button5);
     frame.setSize(200, 200);
     frame.setVisible(true);
   }
